6af97b78cd4a27cb8b1835fce516451d9ee9b256
Kristiyan Zdravkov <kristian.zdravkov@gmail.com>
2019-05-29 12:57:15 +0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDZRmtfr27pIKD/dtsVhh+wyPZxPZQ6tSVALrSDD8shrXHkkP/TAqPx3BlN4APdejAxt3gFidE6Uaj2vKmD4T0Eosk87V10m/vtTf4vQIXbYEyge7d2Jc2iFfXD4FXc+xz5Sp1T/SajE2WcaqJEk6ZV1tJ5DJwpNHHJ9gI8QC0dSOJF4s0VEGQoEZd+Kb/R1VYdFnev9hcmn+6jS+mIOHvOd+9vxpXI//tK3l1mpQv8RymYUyzmpfGhl3yCYSmVbaIor2TlcVssOfAG9iX6AR6NWect2qbYNZsNL0un4PoEu2SztZ2qbcEVqzTjxsSR69r4/UTcWhM1Go59ofe+UHRz',
 	}  
 }
