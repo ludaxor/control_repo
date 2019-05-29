e30f963d3e0b297b4b6eacb56124a485badb2948
Kristiyan Zdravkov <kristian.zdravkov@gmail.com>
2019-05-29 17:51:23 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,21 +4,23 @@ class minecraft {
   }
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-	source => 'https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar',
+    source => 'https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar',
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
   file {'/opt/minecraft/eula.txt:
     ensure => file,
-	content => 'eula=true',
+    content => 'eula=true',
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-	source => 'puppet:///modules/minecraft/minecraft.service',
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }  
 }
